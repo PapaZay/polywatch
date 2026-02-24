1fcec1a3c721edeb498a61519bf1023336c8186e
Isaiah Flager-Hearan <94151429+PapaZay@users.noreply.github.com>
2026-02-24 02:16:06 -0600

    finished marketdetails, also updated signalcard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import {Routes, Route} from "react-router-dom";
 import Layout from "./components/Layout";
 import Dashboard from "./pages/Dashboard.tsx";
 import Calibration from "./pages/Calibration.tsx";
+import MarketDetail from "./pages/MarketDetail.tsx";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/calibration" element={<Calibration />} />
+                <Route path="/markets/:marketId" element={<MarketDetail />} />
             </Routes>
         </Layout>
     )
